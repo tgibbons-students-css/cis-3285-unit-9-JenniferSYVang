6633f28df89a9b9a650bc350b2fecf763938f351
Jennifer Vang <jvang5@css.edu>
2018-11-04 12:53:06 -0600

    Solving Request 405 - I created a private method called WriteToLogFile that takes in two strings and the params object[]. I did this to avoid duplicate code. The LogWarning and LogInfo will call this method after they write to the console. Calling this method will write to a log.txt file located on the users computer.

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
@@ -9,11 +9,22 @@ namespace SingleResponsibilityPrinciple
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
+            WriteToLogFile("WARN", message, args);
         }
 
         public void LogInfo(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("INFO: ", message), args);
+            WriteToLogFile("INFO", message, args);
+        }
+
+        // Solves Request 405 - "As a manager, I want to easily monitor the trading errors my traders make so I can provide them better training to avoid errors." 
+        private void WriteToLogFile(string messageType, string message, params object[] args)
+        {
+            using (StreamWriter w = File.AppendText("log.txt")) // in SingleResponsibilityPrincipleTests/bin/Debug
+            {
+                w.WriteLine("<log><type>" + messageType + "</type><message>" + message + "</message></log>", args);
+            }
         }
     }
 }
